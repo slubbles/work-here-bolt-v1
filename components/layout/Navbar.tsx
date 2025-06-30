6d941f269f48f3ad0f20db5a61038bc4a9ced618
Slubbles <85516773+slubbles@users.noreply.github.com>
2025-06-30 18:06:40 +0800

    Fix JSX syntax error in Navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -439,7 +439,6 @@ export default function Navbar() {
                               >
                                 Disconnect
                               </Button>
-                              </div>
                             </>
                           )}
                           
