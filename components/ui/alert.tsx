227dbc05ba6ff79c8406bca1453a228ceced425f
Slubbles <85516773+slubbles@users.noreply.github.com>
2025-06-30 19:29:36 +0800

    Fix duplicate AlertTitle declaration

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -47,16 +47,4 @@ const AlertTitle = React.forwardRef<
 ))
 AlertTitle.displayName = "AlertTitle"
 
-const AlertDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("text-sm [&_p]:leading-relaxed", className)}
-    {...props}
-  />
-))
-AlertDescription.displayName = "AlertDescription"
-
 export { Alert, AlertDescription, AlertTitle }
