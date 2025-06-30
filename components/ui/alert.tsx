a7098690d2b26cd67a01e64c6085d01a359a83b6
Slubbles <85516773+slubbles@users.noreply.github.com>
2025-06-30 19:30:50 +0800

    Fix missing AlertDescription component

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -47,4 +47,16 @@ const AlertTitle = React.forwardRef<
 ))
 AlertTitle.displayName = "AlertTitle"
 
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
 export { Alert, AlertDescription, AlertTitle }
